0cc048a4dbd9d6292bbbc50dc7dedb836985873b
AytanShabanova <shabanovaaytan@gmail.com>
2024-12-22 11:18:48 +0100

    first commit

---
+++ src/main/java/com/example/renewal/RenewalApplication.java
@@ -0,0 +1,13 @@
+package com.example.renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewalApplication.class, args);
+	}
+
+}
